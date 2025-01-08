4c003d2288251ba5c43f812345093d6ee2c2eff0
krishani <krishani.indrachapa@gmail.com>
2025-01-08 09:43:07 +0000

    create tour

--- tour-service/src/main/java/com/example/Application.java
+++ tour-service/src/main/java/com/example/Application.java
@@ -3,6 +3,7 @@ package com.example;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import com.example.tourservice.TourManagementService;
 import com.example.tourservice.TravelAgentService;
 
 public class Application {
@@ -11,6 +12,9 @@ public class Application {
 
         ApplicationContext context = new AnnotationConfigApplicationContext(Application.class.getPackageName());
         TravelAgentService agent = context.getBean(TravelAgentService.class);
+        TourManagementService tourManagement = context.getBean(TourManagementService.class);
+
+        tourManagement.createTour("Zoo Tour", 100, true);
         System.out.println("\n******Explore California Tour Catalogue******");
         agent.displayTours();
 
